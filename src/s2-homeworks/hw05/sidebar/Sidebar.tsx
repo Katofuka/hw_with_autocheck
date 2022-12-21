7f08d789e66936b668a5e529765106446f2d6c20
Katopuk <ladno4ka@gmail.com>
2022-12-21 21:55:14 +0700

    styles - isActive

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={isActive => isActive ? 'nav.active' : 'nav'} // делает студент
+                        className={params => params.isActive ? s.active : s.nav} // делает студент
+
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={params => params.isActive ? s.active : ''}
                     >
                         Junior
                     </NavLink>
@@ -47,7 +48,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={params => params.isActive ? s.active : ''}
                     >
                         Junior Plus
                     </NavLink>
