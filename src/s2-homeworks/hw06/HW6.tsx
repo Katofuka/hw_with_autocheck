bb1e818c493224280ff32299aa7459eb96a0add8
Katopuk <ladno4ka@gmail.com>
2022-12-22 22:10:43 +0700

    onBlur, onDoubleClick + localstorage

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
-import { restoreState, saveState } from './localStorage/localStorage'
+import {restoreState, saveState} from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
 
 /*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
+ * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, ++onBlurCallback, ++onDoubleClickCallBack
  * 2 - дописать логику функции restore
  * 3 - сделать стили в соответствии с дизайном
  */
@@ -19,7 +19,10 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        const newValue = localStorage.getItem('hw6-editable-span-value')
+        if (newValue) {
+            setValue(JSON.parse(newValue))
+        }
     }
 
     return (
